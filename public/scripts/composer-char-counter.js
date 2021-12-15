e37eab47d7750f55edda4b3da04fa65ecb463798
Jeffery <parkjon8@gmail.com>
2021-12-15 23:55:35 +0000

    Add tweet text counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // Grab the total allowed tweet text counts
+  const totalAllowedCount = $('.counter').val();
+  
+  // Read tweet input and update the remaining character
+  $('#tweet-text').on('input', function() {
+    const typeCount = this.value.length;
+    const remainingCount = totalAllowedCount - typeCount;
+
+    (typeCount > totalAllowedCount) ? $('.counter').css({'color': 'red'}) : $('.counter').css('color', '#545149');
+
+    $('.counter').text(remainingCount);
+  })
+});
