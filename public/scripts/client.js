efe1b690135c3cca37ead7f341d3f2d48e4e1b8b
Jeffery <parkjon8@gmail.com>
2021-12-16 02:02:15 +0000

    Modify details after code review with mentor

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Take in a tweet object
+// Return a tweet <article> element containing the entire HTML structure of the tweet
+const $tweet = $(`<article class="tweet">Hello world</article>`);
