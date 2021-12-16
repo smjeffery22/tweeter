5a9da3ee4e1eea794afea8922241a87c6ec33692
Jeffery <parkjon8@gmail.com>
2021-12-16 07:18:37 +0000

    Serialize the form data and send it to the server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -30,42 +30,46 @@ const tweetData = [
   }
 ];
 
-// Take an array of tweet objects
-// Append each one to #tweet-post-container 
-const renderTweets = function(tweets) {
-  for (const tweet of tweets) {
-    $('#tweet-post-container').prepend(createTweetElement(tweet));
+  // Take an array of tweet objects
+  // Append each one to #tweet-post-container 
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      $('#tweet-post-container').prepend(createTweetElement(tweet));
+    }
+  };
+
+  // Take in a tweet object
+  // Return a tweet <article> element containing the entire HTML structure of the tweet
+  const createTweetElement = function(data) {
+    const $tweet = $(`
+    <article class="tweet">
+      <header class="content-header">
+        <span class="tweeter-name"><img src=${data.user.avatars}> ${data.user.name}</span>
+        <span class="tweeter-id">${data.user.handle}</span>
+      </header>
+      <p class="tweet-content">${data.content.text}</p>
+      <footer class="content-footer">
+        <span class="date-count">${timeago.format(data.created_at)}</span>
+        <div class="footer-icons">
+          <div><i class="fas fa-flag"></i></div>
+          <div><i class="fas fa-retweet"></i></div>
+          <div><i class="fas fa-heart"></i></div>
+        </div>
+      </footer>
+    </article>`);
+
+    return $tweet;
   }
-};
 
-// Take in a tweet object
-// Return a tweet <article> element containing the entire HTML structure of the tweet
-const createTweetElement = function(data) {
-  const $tweet = $(`
-  <article class="tweet">
-    <header class="content-header">
-      <span class="tweeter-name"><img src=${data.user.avatars}> ${data.user.name}</span>
-      <span class="tweeter-id">${data.user.handle}</span>
-    </header>
-    <p class="tweet-content">${data.content.text}</p>
-    <footer class="content-footer">
-      <span class="date-count">${timeago.format(data.created_at)}</span>
-      <div class="footer-icons">
-        <div><i class="fas fa-flag"></i></div>
-        <div><i class="fas fa-retweet"></i></div>
-        <div><i class="fas fa-heart"></i></div>
-      </div>
-    </footer>
-  </article>`);
+  renderTweets(tweetData)
 
-  return $tweet;
-}
-
-renderTweets(tweetData)
-
-// Evenet listener for submit and prevent its default behaviour
-$('#tweet-form').submit(function(event) {
-  event.preventDefault();
-  alert('Submitted!');
-})
+  // Evenet listener for submit and prevent its default behaviour
+  // Serialize the form data and send it to the server
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    alert('Submitted!');
+    const serializedData = $(this).serialize();
+    
+    $.post('/tweets/', serializedData)
+  })
 })
